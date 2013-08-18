84b4ad0fc081cba44013543f55a9d764021f1c21
Mukund Krishnamurthy <mukund.kri@gmail.com>
2013-08-18 15:58:41 +0530

    Added category generator plugin

---
+++ _plugins/categories.rb
@@ -0,0 +1,35 @@
+# All category related tags and generators.
+
+module Jekyll
+
+  module CategoryFilters    
+    def category_linkify(name)
+      %Q{<a href="/categories.html\##{name}">#{name}</a>}
+    end
+  end
+
+  
+  class CategoryPage < Page
+    def initialize(site, base, dir, categories)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = "categories.html"
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'categories_layout.html')
+    end
+  end
+
+  class CategoryPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      site.pages << CategoryPage.new(site, site.source, ".", site.categories)
+    end
+  end
+  
+end
+
+Liquid::Template.register_filter(Jekyll::CategoryFilters)
+

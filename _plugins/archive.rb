b05bfaf49259b25f623e75b1bcdc553adb0f96d0
Mukund Krishnamurthy <mukund.kri@gmail.com>
2016-04-06 16:17:21 +0530

    Adding gridle for css grids

--- _plugins/archive.rb
+++ _plugins/archive.rb
@@ -3,21 +3,22 @@
 module Jekyll
 
   class ArchivePage < Page
-    
+
     def initialize(site, base, dir)
       @site = site
       @base = base
       @dir = dir
       @name = "archives.html"
+      by_year = Hash.new
 
-      by_year = site.posts.group_by {|post| post.date.year}
-      by_year.each do |key, value|
+      grouped = site.posts.docs.group_by {|post| post.date.year}
+      grouped.reverse_each do |key, value|
         by_year[key] = value.group_by {|post| post.date.month } # changing hash in loop. not good
       end
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'archive_layout.html')
-      self.data['by_year'] = by_year      
+      self.data['by_year'] = by_year
     end
   end
 
@@ -26,11 +27,11 @@ module Jekyll
 
     def generate(site)
       site.pages << ArchivePage.new(site, site.source, "." )
-    end    
+    end
   end
 
-  
-  module ExtraDateFilters    
+
+  module ExtraDateFilters
     def to_month(month_int)
       Date::MONTHNAMES[month_int]
     end
@@ -39,5 +40,3 @@ module Jekyll
 end
 
 Liquid::Template.register_filter(Jekyll::ExtraDateFilters)
-
-
